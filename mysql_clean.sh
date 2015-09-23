da84346a7a2257a99560de8b1ddb6f358fa7c25e
Jesus Toledano S. <sysadmin@toledanosjesus.com>
2015-09-23 17:25:19 +0200

    Update mysql_clean.sh

--- mysql_clean.sh
+++ mysql_clean.sh
@@ -2,6 +2,22 @@
 
 # Script to delete rows from a mysql database by selecting the number of months to keep.
 
+###############################
+##  Functions
+###############################
+
+function check(){
+  if [ $? == 0 ]; then
+    echo -e $quit
+  else
+    echo -e $error
+  fi  
+}
+
+###############################
+## Variables
+###############################
+
 db="databasename"
 user="username"
 password="yourpassword"
@@ -11,7 +27,12 @@ field="fieldname"
 months="numberofmonthstokeep"
 day=$(date --date="-$months month" +%Y-%m-%d" "%H:%M:%S )
 quit="\nThe script has finished correctly.\nThe rows deleted are from before $day.\n"
+error="\nERROR - We are sorry, your script failed.\n"
+
+###############################
+## Program
+###############################
 
 mysql $db -u$user -p$password<<<"$action FROM $table WHERE $field < '$day'"
 
-echo -e $quit
+check
