e871527ae1841e5050e66aa2728cb95cc123a72b
Jesus Toledano S. <sysadmin@toledanosjesus.com>
2015-09-23 13:03:18 +0200

    Create mysql_clean.sh

---
+++ mysql_clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script to delete rows from a mysql database by selecting the number of months to keep.
+
+db="databasename"
+user="username"
+password="yourpassword"
+action="DELETE"
+table="tablename"
+field="fieldname"
+months="1"
+day=$(date --date="-$months month" +%Y-%m-%d" "%H:%M:%S )
+quit="\nThe script has finished correctly.\nThe rows deleted are from before $day.\n"
+
+mysql $db -u$user -p$password<<<"$action FROM $table WHERE $field < '$day'"
+
+echo -e $quit
